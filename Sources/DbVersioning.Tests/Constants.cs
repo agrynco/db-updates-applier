8d85a3eb7d8711b9496a3d2b1214bf3b2f51171d
agrynco <agrynco@gmail.com>
2015-09-01 22:29:49 +0300

    Fixed tests

--- Sources/DbVersioning.Tests/Constants.cs
+++ Sources/DbVersioning.Tests/Constants.cs
@@ -4,10 +4,8 @@
     {
         public static class DbMigrations
         {
-            private const string PATH_TO_MIGRATIONS = "Lib.Data.DbVersioning.Tests.Resources";
-
+            private const string PATH_TO_MIGRATIONS = "DbVersioning.Tests.Resources";
             public const string NUMERIC = PATH_TO_MIGRATIONS + ".Numeric.sql";
-
             public const string MAJOR_MINOR = PATH_TO_MIGRATIONS + ".MajorMinor.sql";
         }
     }
